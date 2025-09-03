14fc9bf74ea7422e4625d24c5662e1b2a91dff6d
Ballursajeed <ballursajeed@gmail.com>
2025-09-03 12:58:53 +0530

    checking .env

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -140,7 +140,7 @@ export const userLogout = async(req,res) => {
     })
     } catch (error) {
          return res.status(500).json({
-                message: "Something went wrong,while logging out user",
+                message: "Something went wrong,please try again later",
                 success: false,
                 error,
             });
