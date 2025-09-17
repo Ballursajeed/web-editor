d171b47221a7d06a0d26be613d988b439fbf4ea2
Ballursajeed <ballursajeed@gmail.com>
2025-09-17 18:41:29 +0530

    get single user API

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -182,3 +182,32 @@ export const checkAuth = async(req,res) => {
             });
     }
 }
+
+export const getUser = async(req,res) => {
+    try {
+        
+        const { id } = req.params;
+
+        const user = await User.findById(id);
+
+        if(!user) {
+            return res.status(404).json({
+                message: "User not Found!",
+                success: false
+            });
+        }
+
+        return res.status(200).json({
+            message: "user fetched successfully!",
+            success: true,
+            user
+        })
+
+    } catch (error) {
+        return res.status(500).json({
+            message: "Something went wrong!",
+            success: false,
+            error: error.message
+        })
+    }
+}
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { checkAuth, register, userLogin, userLogout } from "../controllers/user.controller.js";
+import { checkAuth, getUser, register, userLogin, userLogout } from "../controllers/user.controller.js";
 import { validateUser } from "../middlewares/user.middleware.js";
 
 const route = express();
@@ -9,5 +9,6 @@ route.route("/login").post(userLogin);
 route.route("/logout").post(validateUser,userLogout);
 
 route.route("/auth").get(validateUser,checkAuth);
+route.route("/user/:id").get(validateUser,getUser);
 
 export default route;
