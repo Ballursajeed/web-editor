a5f8107140513452d44983fb3d313382cc44dca8
Ballursajeed <ballursajeed@gmail.com>
2025-09-20 19:27:48 +0530

    get collabe project API endpoint done

--- server/routes/file.route.js
+++ server/routes/file.route.js
@@ -5,6 +5,7 @@ import {
          deleteFile, 
          deleteProject, 
          getAllProjects, 
+         getCollabeProject, 
          getFile, 
          getProject, 
          getProjectByUser, 
@@ -23,6 +24,7 @@ route.route('/get/:id').get(validateUser,getFile);
 route.route('/project/get/:id').get(validateUser,getProject);
 route.route('/project/get').get(validateUser,getAllProjects);
 route.route('/project/user').get(validateUser,getProjectByUser);
+route.route('/project/collabe/:session').get(validateUser,getCollabeProject);
 
 route.route('/add').post(validateUser,createFileOrFolder);
 route.route('/new').post(validateUser,newProject);
