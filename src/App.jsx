cf24bcd894f9c9f0a2cfcae0702bfad70ade8333
Ballursajeed <ballursajeed@gmail.com>
2025-09-21 20:01:43 +0530

    route added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./components/Home/Home";
 import Project from "./components/Project/Project";
 import Register from './components/Register/Register';
 import Login from './components/Login/Login';
+import Collabe from './components/Collabe/Collabe';
 
 const App = () => {
 
@@ -14,6 +15,7 @@ const App = () => {
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
       <Route path="/project/:id" element={<Project />} />
+      <Route path="/project/collabe/:session" element={<Collabe />} />
      </Routes>
     </Router>
   );
