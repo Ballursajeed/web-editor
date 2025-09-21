86f2d7f293ba3063cf8aeb4524064d12887727e3
Ballursajeed <ballursajeed@gmail.com>
2025-09-21 20:01:18 +0530

    collabe UI component wrote

---
+++ src/components/Collabe/Collabe.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router';
+import Explorer from '../Explorer';
+import File from '../File';
+import axios from 'axios';
+import { SERVER } from '../../constants';
+import SelectedFiles from '../selectedFiles/SelectedFiles';
+
+const Collabe = () => {
+
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [selectedFiles,setSelectedFiles] = useState([]);
+  const [name,setName] = useState('');
+  const { session } = useParams(); 
+
+  useEffect(() => {
+      const fetchProject = async() => {
+        const res = await axios.get(`${SERVER}/file/project/collabe/${session}`,{
+          withCredentials: true
+        });
+        if(res.data.success){
+            setName(res.data.project.name);
+        }
+      }
+      fetchProject();
+  },[]);
+
+  
+
+  return (
+   <div className="app-container">
+      <Explorer name={name} 
+      projectId={id} 
+      onFileSelect={setSelectedFile} 
+      onFilesSelect={setSelectedFiles}
+      selectedFiles={selectedFiles}
+      />
+
+      <div className='right-container'>
+        <SelectedFiles selectedFiles={selectedFiles} onFilesSelect={setSelectedFiles}  onFileSelect={setSelectedFile} />
+        <File fileId={selectedFile} />
+      </div>
+     </div>
+  )
+}
+
+export default Collabe
